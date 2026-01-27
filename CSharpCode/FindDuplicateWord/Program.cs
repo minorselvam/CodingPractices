02978f87a0c76500108217d5059692f2d6543c29
minorselvam <galaxy.mskumar@gmail.com>
2026-01-27 17:34:32 +0530

    Find duplicate word example code added.

---
+++ CSharpCode/FindDuplicateWord/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//string sentence = "red white black white green yellow red red black white test";
+//var result = string.Join(" ", sentence.Split(' ').Distinct());
+//Console.WriteLine(result);
+
+Console.WriteLine("test1" + Environment.NewLine + "test2" + Environment.NewLine);
