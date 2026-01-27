02a69341ce7fb5b064526fc4d43d7c445ba172e8
minorselvam <galaxy.mskumar@gmail.com>
2026-01-27 17:30:43 +0530

    Async Await example code added.

---
+++ CSharpCode/AsyncAwait/AsyncAwait/Helper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    public class Helper
+    {
+         
+        //With Async and Await
+        public async Task<string> MethodAPI()
+        {
+            await Task.Delay(5000);
+            return "MethodAPI Result";
+        }
+
+        public async Task<string> MethodDB()
+        {
+            await Task.Delay(2000); // Simulate delay
+            return "MethodDB Result";
+        }
+
+        public async Task<string> MethodFileSystem()
+        {
+            // Simulate asynchronous operation (e.g., reading from a file)
+            await Task.Delay(4000); // Simulate delay
+            return "MethodFileSystem Result";
+        }
+         
+
+
+        /*
+        public string MethodAPI()
+        {
+            Task.Delay(1000);
+            return "MethodAPI Result";
+        }
+
+        public string MethodDB()
+        {
+            Task.Delay(2000); // Simulate delay
+            return "MethodDB Result";
+        }
+        public string MethodFileSystem()
+        {
+            // Simulate asynchronous operation (e.g., reading from a file)
+            Task.Delay(4000); // Simulate delay
+            return "MethodFileSystem Result";
+        }
+        */
+    }
+}
---
+++ CSharpCode/AsyncAwait/AsyncAwait/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    internal class Program
+    {
+        
+        //With Async and Await
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Helper objHelper = new Helper();
+            Console.WriteLine("Method 1 called");
+            var a = await objHelper.MethodAPI();
+            Console.WriteLine("Waiting for Method 1 reult: first test message");
+            Console.WriteLine("Waiting for Method 1 reult: second test message");
+
+            Console.WriteLine("Method 1 result: " + a);
+            Console.WriteLine("Method 1 finished");
+
+            var b = await objHelper.MethodDB();
+            Console.WriteLine("Method 2 result: " + b);
+            Console.WriteLine("Method 2 finished");
+
+            var c = await objHelper.MethodFileSystem();
+            Console.WriteLine("Method 3 result: " + c);
+            Console.WriteLine("Method 3 finished");
+
+            Console.WriteLine("All methods and functions are finished");
+        }
+         
+
+        /*
+        //Without Async and Await
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Helper objHelper = new Helper();
+            var a = objHelper.MethodAPI();
+            Console.WriteLine("Method 1 result: " + a);
+            Console.WriteLine("Method 1 finished");
+
+            var b = objHelper.MethodDB();
+            Console.WriteLine("Method 2 result: " + b);
+            Console.WriteLine("Method 2 finished");
+
+            var c = objHelper.MethodFileSystem();
+            Console.WriteLine("Method 3 result: " + c);
+            Console.WriteLine("Method 3 finished");
+
+            Console.WriteLine("All methods and functions are finished");
+        }
+        */
+    }
+}
