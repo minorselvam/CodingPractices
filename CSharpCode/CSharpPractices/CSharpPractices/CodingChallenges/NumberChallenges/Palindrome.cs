9c9c633d57de2d06d788ce2e7e8f83993f8e81e8
minorselvam <selvakumar.m@ust.com>
2026-02-19 18:22:48 +0530

    Code changes

--- CSharpCode/CSharpPractices/CSharpPractices/CodingChallenges/NumberChallenges/Palindrome.cs
+++ CSharpCode/CSharpPractices/CSharpPractices/CodingChallenges/NumberChallenges/Palindrome.cs
@@ -29,5 +29,9 @@ namespace CSharpPractices.CodingChallenges.NumberChallenges
         // 426624 - Total digit count is 6.
         // Pair the same numbers like 66, 44 and 22
 
+        //6346458 ==> 66 3 44 5 8
+        int inputNumber = 6346458;
+       
+         
     }
 }
