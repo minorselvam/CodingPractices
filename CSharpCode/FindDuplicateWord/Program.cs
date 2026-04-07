1084bdcc96f31fd12774e459cd9a3564496117db
minorselvam <galaxy.mskumar@gmail.com>
2026-04-07 18:34:24 +0530

    code cleanup.

--- CSharpCode/FindDuplicateWord/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-//string sentence = "red white black white green yellow red red black white test";
-//var result = string.Join(" ", sentence.Split(' ').Distinct());
-//Console.WriteLine(result);
-
-Console.WriteLine("test1" + Environment.NewLine + "test2" + Environment.NewLine);
