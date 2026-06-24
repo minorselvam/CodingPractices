ed02ccd3b109f89edd94c3ac2517573c859ce1bd
minorselvam <galaxy.mskumar@gmail.com>
2026-06-24 21:36:40 +0530

    Method Hiding Or Shadowing Example

---
+++ CSharpCode/OopsCSharp/MethodHidingOrShadowingEx/MethodHidingOrShadowingEx/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodHidingOrShadowingEx
+{
+    public class BaseLogger
+    {
+        // Normal method in the base class
+        public void Log()
+        {
+            Console.WriteLine("BaseLogger Log Method call");
+        }
+    }
+
+    public class FileLogger : BaseLogger
+    {
+        // Hides the base class Log method using 'new'
+        public new void Log()
+        {
+            Console.WriteLine("FileLogger Log Method call");
+        }
+    }
+}
---
+++ CSharpCode/OopsCSharp/MethodHidingOrShadowingEx/MethodHidingOrShadowingEx/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace MethodHidingOrShadowingEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!!!. Welcome to the Method Hiding Example.");
+
+            // Case 1: Base class reference → calls BaseLogger.Log
+            BaseLogger objBaseLogger = new BaseLogger();
+            objBaseLogger.Log();
+            // Output: BaseLogger Log Method call
+
+            // Case 2: Derived class reference → calls FileLogger.Log
+            FileLogger objFileLogger = new FileLogger();
+            objFileLogger.Log();
+            // Output: FileLogger Log Method call
+
+            // Case 3: Base class reference pointing to derived object
+            // Because of method hiding, this still calls BaseLogger.Log
+            BaseLogger baseLogger = new FileLogger();
+            baseLogger.Log();
+            // Output: BaseLogger Log Method call
+        }
+    }
+}
