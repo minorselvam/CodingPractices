15f67d09e1cebbe931adeb8188207e3329c7bd94
minorselvam <galaxy.mskumar@gmail.com>
2026-06-30 14:59:15 +0530

    Azure Event Hub - Shipping Service.

---
+++ Azure-Practices/AzureEventHub/AzEvHbShippingService/AzEvHbShippingService/Controllers/ShippingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SharedContracts;
+
+namespace AzEvHbShippingService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ShippingController : ControllerBase
+{
+    [HttpGet("{orderId}")]
+    public IActionResult GetShippingStatus(Guid orderId)
+    {
+        // For demo purposes, just return a dummy status
+        return Ok($"Shipping started for Order {orderId}");
+    }
+}
---
+++ Azure-Practices/AzureEventHub/AzEvHbShippingService/AzEvHbShippingService/Services/EventHubConsumer.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Messaging.EventHubs.Consumer;
+using SharedContracts;
+using System.Text.Json;
+
+namespace AzEvHbShippingService.Services;
+
+public class EventHubConsumer : BackgroundService
+{
+    private readonly IConfiguration _config;
+
+    public EventHubConsumer(IConfiguration config) => _config = config;
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var consumer = new EventHubConsumerClient(
+            EventHubConsumerClient.DefaultConsumerGroupName,
+            _config["EventHub:ConnectionString"],
+            _config["EventHub:Name"]);
+
+        await foreach (var evt in consumer.ReadEventsAsync(stoppingToken))
+        {
+            var payment = JsonSerializer.Deserialize<Payment>(
+                evt.Data.EventBody.ToString()
+            );
+
+            if (payment?.Success == true)
+            {
+                Console.WriteLine($"Shipping started for Order {payment.OrderId}");
+            }
+        }
+    }
+}
