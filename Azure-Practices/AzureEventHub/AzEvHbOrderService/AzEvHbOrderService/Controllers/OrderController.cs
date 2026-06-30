9acbae578d1d130b70572b3435112ef0528cca9e
minorselvam <galaxy.mskumar@gmail.com>
2026-06-30 13:22:52 +0530

    Azure Event Hub POC - Order Service.

---
+++ Azure-Practices/AzureEventHub/AzEvHbOrderService/AzEvHbOrderService/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SharedContracts;
+using AzEvHbOrderService.Services;
+
+namespace AzEvHbOrderService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OrderController : ControllerBase
+{
+    private readonly EventHubProducer _producer;
+
+    public OrderController(EventHubProducer producer) => _producer = producer;
+
+    [HttpPost]
+    public async Task<IActionResult> CreateOrder([FromBody] Order order)
+    {
+        await _producer.PublishAsync(order);
+        return Ok($"Order {order.Id} published");
+    }
+}
---
+++ Azure-Practices/AzureEventHub/AzEvHbOrderService/AzEvHbOrderService/Services/EventHubProducer.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using Microsoft.AspNetCore.Mvc.Diagnostics;
+using System.Text.Json;
+
+namespace AzEvHbOrderService.Services;
+
+public class EventHubProducer
+{
+    private readonly EventHubProducerClient _client;
+
+    public EventHubProducer(IConfiguration config)
+    {
+        _client = new EventHubProducerClient(
+            config["EventHub:ConnectionString"],
+            config["EventHub:Name"]);
+    }
+
+    public async Task PublishAsync<T>(T message)
+    {
+        using EventDataBatch batch = await _client.CreateBatchAsync();
+        batch.TryAdd(new Azure.Messaging.EventHubs.EventData(
+            JsonSerializer.SerializeToUtf8Bytes(message)
+        ));
+        await _client.SendAsync(batch);
+    }
+}
